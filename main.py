5d641ba87a87b9b9edd7a38cfe5b1bd32769c9cf
Kgo890 <gorekaden501@gmail.com>
2025-02-08 15:07:55 -0800

    Made the shuffle function and a generator to generate the passwords

--- main.py
+++ main.py
@@ -8,20 +8,21 @@ def shuffle(string):
     return ''.join(tempList)
 
 
-# Main program starts here
-uppercaseLetter1 = chr(random.randint(65, 90))  # Generate a random Uppercase letter (based on ASCII code)
-uppercaseLetter2 = chr(random.randint(65, 90))
-lowercaseLetter1 = chr(random.randint(97, 122))  # Generate a random lowercase letter (based on ASCII code)
-lowercaseLetter2 = chr(random.randint(97, 122))
-digit1 = chr(random.randint(48, 57))  # Generate a random digit (from 0 to 9) (based on ASCII code)
-digit2 = chr(random.randint(48, 57))
-punctuationSign1 = chr(random.randint(33, 47))   # Generate a random punctuation sign (based on ASCII code)
-punctuationSign2 = chr(random.randint(58, 64))
+# a function to generate a random int, or char
+def generator():
+    uppercaseLetter1 = chr(random.randint(65, 90))  # Generate a random Uppercase letter (based on ASCII code)
+    uppercaseLetter2 = chr(random.randint(65, 90))
+    lowercaseLetter1 = chr(random.randint(97, 122))  # Generate a random lowercase letter (based on ASCII code)
+    lowercaseLetter2 = chr(random.randint(97, 122))
+    digit1 = chr(random.randint(48, 57))  # Generate a random digit (from 0 to 9) (based on ASCII code)
+    digit2 = chr(random.randint(48, 57))
+    punctuationSign1 = chr(random.randint(33, 47))  # Generate a random punctuation sign (based on ASCII code)
+    punctuationSign2 = chr(random.randint(58, 64))
 
-# Generate password using all the characters, in random order
-password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + lowercaseLetter2 + digit1 + digit2 + punctuationSign1 + punctuationSign2
-password = shuffle(password)
+    password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + lowercaseLetter2 + digit1 + digit2 + punctuationSign1 + punctuationSign2
+    return shuffle(password)
+
 
 # Ouput
 print("Welcome to the password generator, that generates a 8 character password for you to use:")
-print(f"Your new password is: {password}")
+print(f"Your new password is: {generator()}")
